3397617449fe1928b0cfa56d949f8327874f09dc
imkiran22 <kiran22292@gmail.com>
2024-01-09 21:19:30 +0530

    Jest test case fixes

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -2,5 +2,6 @@
 import gameBoard from "./gameBoard";
 
 test("tests gameboard", () => {
-  expect(gameBoard("hi")).toMatch("hello");
+  const gameGrid = gameBoard(2, 2);
+  expect(gameGrid.outerHTML).toContain('<div style="--grid-rows: 2; --grid-cols: 2;"><div class="grid-item"></div><div class="grid-item"></div><div class="grid-item"></div><div class="grid-item"></div></div>');
 });
